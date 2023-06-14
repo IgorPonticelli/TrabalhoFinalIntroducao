5b73bbfea09e187198d18b24113b6b4bcf1f9f35
igor Ponticelli <igorponticelli8@gmail.com>
2023-06-14 12:04:25 -0300

    Layout1

--- TrabalhoFinal/src/CadastroPacientes.java
+++ TrabalhoFinal/src/CadastroPacientes.java
@@ -66,7 +66,7 @@ public class CadastroPacientes {
             }
         }
         else{
-            System.out.println("\n Nenhum paciente encontrado");
+            System.out.println("\n **Nenhum paciente encontrado**");
         }
     }
 
